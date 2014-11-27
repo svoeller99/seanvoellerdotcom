579ab86f74dd1bce7e3804e1f0ad64e18137cbb1
Sean Voeller <sean.voeller@gmail.com>
2014-11-27 15:55:22 -0800

    initial

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name seanvoellerdotcomApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the seanvoellerdotcomApp
+ */
+angular.module('seanvoellerdotcomApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
